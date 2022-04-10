c1a891cc114ab783ebb8f11d8e565fb68d04f44c
Anatoliy <tolxpams@gmail.com>
2022-04-10 13:24:56 +0300

    navigation controller

--- FanBase/Controller/MoviesVC.swift
+++ FanBase/Controller/MoviesVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MoviesVC.swift
 //  FanBase
 //
 //  Created by Anatoliy Khramchenko on 10.04.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MoviesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
