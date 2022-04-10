1d41ae545fe733d4f35542740d34f929ac0fb967
Anatoliy <tolxpams@gmail.com>
2022-04-10 20:09:28 +0300

    actorVC

---
+++ FanBase/Controller/ActorVC.swift
@@ -0,0 +1,34 @@
+//
+//  ActorVC.swift
+//  FanBase
+//
+//  Created by Anatoliy Khramchenko on 10.04.2022.
+//
+
+import UIKit
+
+class ActorVC: UIViewController {
+    
+    
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var countryFlag: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
